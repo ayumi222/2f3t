2dd62d27054ff06270d744a42d5b43c90c327140
ayumi222 <108939482+ayumi222@users.noreply.github.com>
2023-10-02 08:11:09 -0300

    Add files via upload

---
+++ 2f3t-main/js/script.js
@@ -0,0 +1,53 @@
+mensagem = (msg) => alert (msg);
+
+soma = (a,b) => a+b;
+sub = (a,b) => a-b;
+mult = (a,b) => a*b;
+div = (a,b) => a/b;
+raiz = x => Math.sqrt(x);
+let a = "";
+let b = "";
+let valor = "";
+let temPonto = false;
+//função para mostrar numero no display 
+function escrever_display(num){
+    document.getElementById("resultado").value = num;
+}
+function digitando(tecla) {
+    valor += tecla;
+    escrever_display(valor);
+}
+
+function digitando(tecla){
+    if( tecla =="."){
+        if(!temPonto){
+            temPonto = true;
+            valor += tecla;
+            escrever_display(valor);
+        }
+        return;
+    }
+    valor += tecla;
+    escrever_display(valor);
+}
+let executa = "";
+function operacao (op){
+    executa = op;
+    a = valor;
+    valor = "";
+    temPonto = false;
+}
+function calcula(){
+    if(executa != ""){
+        b = valor;
+        if(executa == "mult") escrever_display(mult(a,b));
+        if(executa == "div") escrever_display(div(a,b));
+        if(executa == "sub") escrever_display(sub(a,b));
+        if(executa == "soma") escrever_display(soma(number(a),number(b)));
+        a = "";
+        b = "";
+        valor = "";
+        temponto = false;
+    }
+}
+
